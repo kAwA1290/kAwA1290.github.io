7973d7fb896aa8c4f80c4d264e058e5569040e88
kAwA1290 <m21037@g.metro-cit.ac.jp>
2023-02-27 23:51:42 +0900

    publicatable: final

--- components/Works.tsx
+++ components/Works.tsx
@@ -9,7 +9,7 @@ const Works = () => {
 		<div className="flex flex-auto flex-col items-center my-5">
 			<p className="text-3xl text-stone-150 font-semibold mb-5">Works</p>
 			<div className="flex flex-col md:flex-row items-stretch w-1/2">
-				<Artifact link="https://github.com/kAwA1290/AirMouse" imagePath={demo_gif.src} title="AirMouse" detail="非接触で未来的なマウスはあなたの手です。"/>
+				<Artifact link="https://github.com/kAwA1290/AirMouse" imagePath={demo_gif.src} title="AirMouse" detail="非接触で未来的なマウスはあなたの手です。Powered by mediapipe."/>
 				<Artifact link="https://github.com/kAwA1290/myclib" imagePath={clib_picture.src} title="Myclib" detail="BSD Manualに準拠しています。(一部を除く)"/>
 
 				<Artifact link="https://github.com/kAwA1290/nengajo" imagePath={nengajo_picture.src} title="Nengajo" detail="Esolangで年賀状を書く。"/>
