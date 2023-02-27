34cfcaaca0d9b29ebaf114859aecebe97a5e6245
kAwA1290 <m21037@g.metro-cit.ac.jp>
2023-02-27 19:36:45 +0900

    publicatable: mobile support

--- components/Artifact.tsx
+++ components/Artifact.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Artifact = ({ link, imagePath, title, detail }) => {
    return (
-      <a href={link} target="_blank" rel="noopener noreferrer" className="border-2 border-stone-700 overflow-hidden w-full rounded-md">
+      <a href={link} target="_blank" rel="noopener noreferrer" className="border-2 border-stone-700 overflow-hidden w-full rounded-md mx-1 my-1">
          <img src={imagePath} className="w-full h-36 md:h-48 object-cover cursor-pointer"/>
          <div className="w-full p-4">
             <h3 className="text-lg md:text-xl text-stone-150 mb-2 md:mb-3 font-semibold">{title}</h3>
--- components/Works.tsx
+++ components/Works.tsx
@@ -6,7 +6,7 @@ const Works = () => {
 	return (
 		<div className="flex flex-col items-center my-5">
 			<p className="text-3xl text-stone-150 font-semibold mb-5">Works</p>
-			<div className="flex flex-col md:flex-row items-left space-x-1 w-1/2">
+			<div className="flex flex-col md:flex-row items-center w-1/2">
 				<Artifact link="https://github.com/kAwA1290/AirMouse" imagePath={picture.src} title="AirMouse" detail="非接触で未来的なマウスはあなたの手です。"/>
 				<Artifact link="https://github.com/kAwA1290/myclib" imagePath={picture.src} title="myclib" detail="BSD Manualに準拠しています。(一部を除く)"/>
 			</div>
