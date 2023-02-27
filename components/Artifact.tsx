e99d51851152f6ac7ca32f9d29159040cc7b21be
kAwA1290 <m21037@g.metro-cit.ac.jp>
2023-02-27 16:39:55 +0900

    publicatable: fix margins

--- components/Artifact.tsx
+++ components/Artifact.tsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const Artifact = ({ link, imagePath, title, detail }) => {
    return (
-      <a href={link} target="_blank" rel="noopener noreferrer" className="border-2 border-stone-700 overflow-hidden w-full shadow-lg">
+      <a href={link} target="_blank" rel="noopener noreferrer" className="border-2 border-stone-700 overflow-hidden w-full rounded-md">
          <img src={imagePath} className="w-full h-36 md:h-48 object-cover cursor-pointer"/>
          <div className="w-full p-4">
-            <h3 className="text-lg md:text-xl dark:text-white mb-2 md:mb-3 font-semibold ">{title}</h3>
+            <h3 className="text-lg md:text-xl dark:text-white mb-2 md:mb-3 font-semibold">{title}</h3>
 			<p className="text-sm max-w-xl">{detail}</p>
          </div>
       </a>
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ import github_picture from '../assets/images/github.png'
 
 const Footer = () => {
 	return (
-		<div className="flex flex-col items-center bg-stone-800 mt-20">
+		<div className="flex flex-col items-center bg-stone-800 mt-10">
 			<a href="https://github.com/kAwA1290" target="_blank" rel="noopener noreferrer" className="overflow-hidden w-10 shadow-lg">
 				<img src={github_picture.src} className="w-full w-10 cursor-pointer"/>
 			</a>
