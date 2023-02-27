ccc7a7e1b4c8fc53910877627172155aed174ae2
kAwA1290 <m21037@g.metro-cit.ac.jp>
2023-02-27 16:49:16 +0900

    publicatable: fix text color

--- components/Artifact.tsx
+++ components/Artifact.tsx
@@ -5,7 +5,7 @@ const Artifact = ({ link, imagePath, title, detail }) => {
       <a href={link} target="_blank" rel="noopener noreferrer" className="border-2 border-stone-700 overflow-hidden w-full rounded-md">
          <img src={imagePath} className="w-full h-36 md:h-48 object-cover cursor-pointer"/>
          <div className="w-full p-4">
-            <h3 className="text-lg md:text-xl dark:text-white mb-2 md:mb-3 font-semibold">{title}</h3>
+            <h3 className="text-lg md:text-xl text-stone-150 mb-2 md:mb-3 font-semibold">{title}</h3>
 			<p className="text-sm max-w-xl">{detail}</p>
          </div>
       </a>
--- components/Timeline.tsx
+++ components/Timeline.tsx
@@ -8,10 +8,10 @@ const Timeline = ({ time, title, belong, belongColor, detail}) => {
 				<div className="absolute w-3 h-3 bg-stone-600 rounded-full mt-1.5 -left-1.5 border border-stone-800" />
 					<div className="flex flex-wrap gap-4 flex-row items-left test-xs">
 						<span className="inline-block px-2 py-1 font-bold text-stone-900 bg-stone-300 rounded-md">{time}</span>
-						<h3 className="inline-block text-lg font-semibold text-stone-200">{title}</h3>
+						<h3 className="inline-block text-lg font-semibold text-stone-150">{title}</h3>
 						<span className={belongClass}>{belong}</span>
 					</div>
-				<p className="my-2 text-base text-stone-400">{detail}</p>
+				<p className="my-2 text-base text-stone-150">{detail}</p>
 			</li>
 		</ol>
 	)
