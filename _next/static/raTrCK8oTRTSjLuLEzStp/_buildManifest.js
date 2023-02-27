6ded26872acd0e94de3677ae76abcee2abf1e4eb
kAwA1290 <kAwA1290@users.noreply.github.com>
2023-02-27 07:51:39 +0000

    deploy: 056ad67203e860baa0c2c3e923fe51024ceb236b

---
+++ _next/static/raTrCK8oTRTSjLuLEzStp/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-4e79e1e98fce7a2b.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
